f8f1ae734b4b481789c318cefe78aaa877fdcec5
Harsh Raj <130693037+Harsh2749@users.noreply.github.com>
2025-07-26 16:11:28 +0530

    Update auth.js

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-
+const JWT_SECRET = process.env.JWT_SECRET || "secret_key";
 const auth = (req, res, next) => {
   const token = req.headers.authorization?.split(" ")[1];
   if (!token) return res.status(401).json({ message: "Unauthorized" });
